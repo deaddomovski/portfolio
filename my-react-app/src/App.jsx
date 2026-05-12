c0abb2db922e56e5490e36f20690199d408c927d
deaddomovski <kozar.H8ME@gmail.com>
2026-05-12 23:30:37 +0200

    more function

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,7 +4,6 @@ function App() {
       <h1>Привет, React!</h1>
       <p>Мой первый компонент</p>
     </div>
-  )dloldldf;ermkglmrqegoregkregmergmreglregrermk;gmkrgm
-}
+  )dloldldf;ermkglmrqegoregkreg
 
 export default App
