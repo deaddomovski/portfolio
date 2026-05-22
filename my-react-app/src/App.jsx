24b27490e7e5cc3a5995c7c305879c79093923e7
deaddomovski <kozar.H8ME@gmail.com>
2026-05-22 21:05:18 +0200

    fix bugs

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,6 +4,6 @@ function App() {
       <h1>Привет, React!</h1>
       <p>Мой первый компонент</p>
     </div>
-  )dlolflflflflfflfld
+  )dlolflflflflfflfldfkegfoeofefegf
 
 export default Appfweferfewfgit add .
