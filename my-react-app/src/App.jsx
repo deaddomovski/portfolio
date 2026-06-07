59c29207afbfb3e3f25c43cfea3509f2c9df25d9
deaddomovski <kozar.H8ME@gmail.com>
2026-06-07 19:42:10 +0400

    add Button component with props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,15 @@
+import Button from './Button'
+
 function App() {
   return (
     <div>
       <h1>Привет, React!</h1>
       <p>Мой первый компонент</p>
+      <Button text="Сохранить" color="green"/>
+      <Button text="Удалить" color="gray"/>
+      <Button text="Отмена" color="red"/>
     </div>
-  )dddddefefeffeef
+  )
 }
 
 export default App
