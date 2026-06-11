7550d672e552701962cd8cbc551fbc4096ef9002
deaddomovski <kozar.H8ME@gmail.com>
2026-06-12 00:22:11 +0400

    add task input with controlled component

--- my-react-app/src/TodoList.jsx
+++ my-react-app/src/TodoList.jsx
@@ -1,13 +1,26 @@
-import { useState} from "react";
+import { useEffect, useState} from "react";
 
 function TodoList() {
     const [tasks, setTasks] = useState(['Learning React','Make project', 'Push on GitHub'])
+    const [newTask, setNewTask] = useState('')
+
+    function AddTask(){
+            setTasks([ ...tasks , newTask])
+            setNewTask('')
+    }
     return (
-        <ul>
-            {tasks.map((task, index) => (
-                <li key={index}>{task}</li>
-            ))}
-        </ul>
+        <div>
+            <input 
+                value={newTask}
+                onChange={(e) => setNewTask(e.target.value)}
+            />
+            <ul>
+                {tasks.map((task, index) => (
+                    <li key={index}>{task}</li>
+                ))}
+            </ul>
+            <button onClick={AddTask}>Add new task</button>
+        </div>
     )
 }
 
