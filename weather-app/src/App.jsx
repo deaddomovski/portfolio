485d5dfabac65590b2f5ccd4cd92fde75010b52d
deaddomovski <kozar.H8ME@gmail.com>
2026-08-06 08:45:05 +0400

    add weather app with OpenWeatherMap API

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+function App() {
+  const [ city, setCity ] = useState('')
+  const [ weather, setWeather] = useState(null)
+  const [loading, setLoading ] = useState(false)
+  const [error, setError ] = useState(null)
+
+  function getWeather () {
+    if (city.trim() === '') return
+      setLoading(true)
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8691a3c9579f0f3877cabec0fc06dd21&units=metric`)
+    .then(response => {
+      console.log(response.data)
+      setWeather(response.data)
+      setLoading(false)
+    })
+    .catch(() => {
+      setError('City dont find')
+      setLoading(false)
+    })
+
+  }
+
+    return (
+      <div>
+        <input value ={city} onChange={(e)=> setCity(e.target.value)} /> 
+        <button onClick={getWeather}>Find</button>
+        <h1>Weather App</h1>
+
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        {weather && (
+          <div>
+            <h2>{weather.name}</h2>
+            <p>Temperature: {Math.round(weather.main.temp)}°C</p>
+            <p>Weather: {weather.weather[0].description}</p>
+            <p>Humidity: {weather.main.humidity}%</p>
+          </div>
+          )}
+          </div>
+    )
+}
+
+export default App
