aef05383f0e34b16695cd4b306ab445d018a17af
Alexey Kozar <kozar.h8me@gmail.com>
2026-09-04 09:09:13 +0400

    add FilterBar component with cuisine filtering

---
+++ foodmap/src/components/FilterBar.tsx
@@ -0,0 +1,20 @@
+type FilterBarProps = {
+    selected: string
+    onSelect: (cuisine:string) => void
+}
+
+function FilterBar({selected, onSelect}: FilterBarProps) {
+    const cuisines = [ 'All', 'Georgian', 'Italian','Japanese' ] 
+
+    return (
+    <div>
+        {cuisines.map((cuisine) => (
+            <button key={cuisine} onClick={() => onSelect(cuisine)}>
+                {cuisine}
+            </button>
+        ))}
+    </div>
+)
+}
+
+export default FilterBar
--- foodmap/src/pages/Home.tsx
+++ foodmap/src/pages/Home.tsx
@@ -1,11 +1,19 @@
 import { restaurants } from '../data/restaurants'
 import RestaurantCard from '../components/RestaurantCard'
+import { useState } from "react" ;
+import FilterBar from '../components/FilterBar';
 
 function Home() {
+    const [selected, setSelected] = useState('All')
+    const filtered = selected === 'All'
+    ? restaurants
+    : restaurants.filter(r => r.cuisine === selected)
+
 return (
         <div>
-            {restaurants.map((restaurant) =>(
-                <RestaurantCard key = {restaurant.id} restaurant={restaurant} />
+            <FilterBar selected={selected} onSelect={setSelected} />
+                {filtered.map((restaurant) => (
+            <RestaurantCard key={restaurant.id} restaurant={restaurant} />
             ))}
         </div>
     )
