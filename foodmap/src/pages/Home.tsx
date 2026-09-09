6b323f4a7389e1d2a2d9ce14dbda6c7817fd2343
Alexey Kozar <kozar.h8me@gmail.com>
2026-09-09 08:12:27 +0400

    add budget filter on Home and RestaurantPage

--- foodmap/src/pages/Home.tsx
+++ foodmap/src/pages/Home.tsx
@@ -2,15 +2,20 @@ import { restaurants } from '../data/restaurants'
 import RestaurantCard from '../components/RestaurantCard'
 import { useState } from "react" ;
 import FilterBar from '../components/FilterBar';
+import { dishes } from '../data/dishes';
 
 function Home() {
     const [selected, setSelected] = useState('All')
-    const filtered = selected === 'All'
-    ? restaurants
-    : restaurants.filter(r => r.cuisine === selected)
+    const [budget, setBudget] = useState(0)
+    const filtered = restaurants
+    .filter(r => selected === 'All' || r.cuisine === selected)
+    .filter(r => budget === 0 || dishes.some(d => d.restaurantId === r.id && d.price <= budget))
+
 
 return (
         <div>
+            <label htmlFor="budget">Your budget: </label>
+            <input id="budget" type="number" value={budget} onChange={(e) => setBudget(Number(e.target.value))}></input>
             <FilterBar selected={selected} onSelect={setSelected} />
                 {filtered.map((restaurant) => (
             <RestaurantCard key={restaurant.id} restaurant={restaurant} />
--- foodmap/src/pages/RestaurantPage.tsx
+++ foodmap/src/pages/RestaurantPage.tsx
@@ -1,22 +1,30 @@
 import { useParams } from "react-router-dom";
 import { restaurants } from "../data/restaurants";
 import { dishes } from '../data/dishes'
+import { useState } from "react"
 
 function RestaurantPage() {
     const { id } = useParams()
+    const [budget, setBudget] = useState(0)
     const restaurant = restaurants.find(r => r.id === Number(id))
     if (!restaurant) return <p>Ресторан не найден</p>
     const restaurantDishes = dishes.filter(d => d.restaurantId === restaurant.id)
+    const budgetDishes = restaurantDishes.filter(d => d.price <= budget)
 
     return (
         <div>
+            <input 
+                type="number" 
+                value={budget}
+                onChange={(e) => setBudget(Number(e.target.value))}
+                placeholder="Введи бюджет"/>
             <h1>{restaurant.name}</h1>
             <p>{restaurant.address}</p>
             <p>{restaurant.description}</p>
             <p>Rating: {restaurant.rating}</p>
             <ul>
-                {restaurantDishes.map(dish => (
-                    <li key={dish.id}>{dish.name} — {dish.price}₾</li>
+                {budgetDishes.map(dish => (
+                <li key={dish.id}>{dish.name} — {dish.price}₾</li>
                 ))}
             </ul>
         </div>
